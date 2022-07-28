d3038152ef71395a556d0ff981bb71610b551f6d
yeonsu <drawingbonny@gmail.com>
2022-07-28 23:10:30 +0900

    Add code practice

---
+++ iOS-Study/practiceCode.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+print(greeting)
+
+// ======= < 변수 선언 > =======
+
+let name: String = "yeonsu Kim"     //상수
+var age: Int = 24                   //변수
+let birth: Int = 990712
+
+//문자열 보관법
+print("Hi! My name is \(name) and I'm \(age)years old")
+
+
